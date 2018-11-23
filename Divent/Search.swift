37fd8274a7c500930008a28053315f20f56af0c3
Michael Weir and Filipe Castanheira <macosuser@new-macos-mac.local>
2018-11-23 18:12:39 -0500

    Initial Commit

---
+++ Divent/Search.swift
@@ -0,0 +1,9 @@
+//
+//  Search.swift
+//  Divent
+//
+//  Created by Michael Weir on 11/23/18.
+//  Copyright © 2018 Michael Weir & Filipe Castanheira. All rights reserved.
+//
+
+import Foundation
---
+++ Divent/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divent
+//
+//  Created by Michael Weir on 11/23/18.
+//  Copyright © 2018 Michael Weir & Filipe Castanheira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
