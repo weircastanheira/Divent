19a666aef937aabd15b257724937459e13e747f3
Workbook <workbook@workbooks-macbook-pro.local>
2018-12-04 16:40:41 -0500

    Firebase is connected to the project. Now we need to implement variables from storyboard so we can store user info into Firebase

--- Divent/AppDelegate.swift
+++ Divent/AppDelegate.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2018 Michael Weir & Filipe Castanheira. All rights reserved.
 //
 
+
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
--- Divent/View Controllers/CreateViewController.swift
+++ Divent/View Controllers/CreateViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    fileprivate var ref : DatabaseReference? // Firebase variable
     @IBOutlet weak var pickerView: UIPickerView!
     
     let pickerData = ["Any","Sports","Gaming","Hangout","Nightlife","Other"]
@@ -30,15 +32,39 @@ class CreateViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         
     }
     
-    
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        Firebase implementation
+        self.ref = Database.database().reference()
+        self.registerForFireBaseUpdates()
+    }
+    
+//    Necessary for Firebase process
+    fileprivate func registerForFireBaseUpdates()
+    {
+        self.ref!.child("history").observe(.value, with: { snapshot in
+            if let postDict = snapshot.value as? [String : AnyObject] {
+                //                TODO: Change this to actual project's values
+                /*
+                var tmpItems = [Conversion]()
+                for (_,val) in postDict.enumerated() {
+                    let entry = val.1 as! Dictionary<String,AnyObject>
+                    let timestamp = entry["timestamp"] as! String?
+                    let origFromVal = entry["origFromVal"] as! Double?
+                    let origToVal = entry["origToVal"] as! Double?
+                    let origFromUnits = entry["origFromUnits"] as! String?
+                    let origToUnits = entry["origToUnits"] as! String?
+                    let origMode = entry["origMode"] as! String?
+                    
+                    tmpItems.append(Conversion(fromVal: origFromVal!, toVal: origToVal!, mode: CalculatorMode(rawValue: origMode!)!, fromUnits: origFromUnits!, toUnits: origToUnits!, timestamp: (timestamp?.dateFromISO8601)!))
+                }
+                */
+//                self.entries = tmpItems
+            }
+        })
         
     }
+
     
 }
