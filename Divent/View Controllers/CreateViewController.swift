440ae071f4409b5ff1ee2c425cf54885aa63ee02
Michael Weir and Filipe Castanheira <macosuser@new-macos-mac.local>
2018-12-08 00:25:33 -0500

    changed all pickers... added functionaility

--- Divent/View Controllers/CreateViewController.swift
+++ Divent/View Controllers/CreateViewController.swift
@@ -9,31 +9,150 @@
 import UIKit
 import Firebase
 
-class CreateViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class CreateViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     fileprivate var ref : DatabaseReference? // Firebase variable
-    @IBOutlet weak var pickerView: UIPickerView!
+
     
-    let pickerData = ["Any","Sports","Gaming","Hangout","Nightlife","Other"]
+    @IBOutlet weak var datePickerTF: UITextField!
+    let datePicker = UIDatePicker()
+    
+    @IBOutlet weak var activityPickerTF: UITextField!
+    let activityPicker = UIPickerView()
+    
+  
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var moreDetails: UITextView!
+    
+    let pickerData = ["Choose...","Sports","Gaming","Hangout","Nightlife","Other"]
     
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerData.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)-> String?{
         return pickerData[row]
-        
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        activityPickerTF.text = pickerData[row]
+    }
+    
+    func createActivityPicker(){
+        activityPickerTF.inputView = activityPicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneActivityClicked))
+        toolbar.setItems([doneButton], animated: true)
+        activityPickerTF.inputAccessoryView = toolbar
+        
         
     }
     
+    func createDatePicker(){
+        let thisYear = Calendar.current.component(.year, from: Date())
+        let todaysDate = Date()
+        datePicker.datePickerMode = .date
+        datePicker.minimumDate = todaysDate
+        datePicker.maximumDate = Calendar.current.date(from:DateComponents(year: thisYear+2))
+        datePickerTF.inputView = datePicker
+       
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneDateClicked))
+        
+        toolbar.setItems([doneButton], animated: true)
+        datePickerTF.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneDateClicked(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        datePickerTF.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func doneActivityClicked(){
+        self.view.endEditing(true)
+    }
+    
+    @objc func hidePickers(){
+        self.view.endEditing(true)
+    }
+
+    @IBAction func confirmClicked(_ sender: Any) {
+        // Declare Alert message
+        let dialogMessage = UIAlertController(title: "Confirm", message: "Create this activity?", preferredStyle: .alert)
+        
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            print("Ok button tapped")
+            self.confirmRecord() //firebase here?
+        })
+        
+        // Create Cancel button with action handlder
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
+            print("Cancel button tapped")
+        }
+        
+        //Add OK and Cancel button to dialog message
+        dialogMessage.addAction(ok)
+        dialogMessage.addAction(cancel)
+        
+        // Present dialog message to user
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func confirmRecord()
+    {
+        //if statement if every label is correct types etc / filled out
+        print("Activity being confirmed")
+        //change below to if critera met
+        if(1==1){
+            let dialogMessage = UIAlertController(title: "Activity Confirmed", message: "Your activity has been added", preferredStyle: .alert)
+
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+            })
+
+            dialogMessage.addAction(ok)
+
+            self.present(dialogMessage, animated: true, completion: nil)
+        }
+            //if critera is unmet
+        else{
+            let dialogMessage = UIAlertController(title: "Activity Denied", message: "Please check that all forms are filled correctly", preferredStyle: .alert)
+            
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+            })
+            
+            dialogMessage.addAction(ok)
+            
+            self.present(dialogMessage, animated: true, completion: nil)
+            
+        }
+        
+        // possibly change this to the firebase function
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityPicker.delegate = self
+        createDatePicker()
+        createActivityPicker()
+        
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.self.hidePickers))
+        self.view.addGestureRecognizer(detectTouch)
         
 //        Firebase implementation
         self.ref = Database.database().reference()
@@ -43,7 +162,7 @@ class CreateViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
 //    Necessary for Firebase process
     fileprivate func registerForFireBaseUpdates()
     {
-        self.ref!.child("history").observe(.value, with: { snapshot in
+        self.ref!.child("activities").observe(.value, with: { snapshot in
             if let postDict = snapshot.value as? [String : AnyObject] {
                 //                TODO: Change this to actual project's values
                 /*
--- Divent/View Controllers/SearchViewController.swift
+++ Divent/View Controllers/SearchViewController.swift
@@ -8,63 +8,82 @@
 
 import UIKit
 
+protocol searchDelegation{
+    func settingsChanged(activityText: String, mileSliderValue: String)
+}
+
 class SearchViewController:UIViewController,UIPickerViewDataSource,UIPickerViewDelegate  {
 
     
     @IBOutlet weak var mileSliderValue: UILabel!
     
 
-    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var activityPickerTF: UITextField!
+    let activityPicker = UIPickerView()
     
     
-    @IBOutlet weak var activityText: UILabel!
-    
-    @IBOutlet weak var setButton: UIButton!
-    
-    @IBAction func changeButton(_ sender: UIButton) {
-        pickerView.isHidden = false
-        setButton.isHidden = false
-    }
-    
-    
-    @IBAction func setClicked(_ sender: UIButton) {
-        pickerView.isHidden = true
-        setButton.isHidden = true
-    }
-    
-    
+    var delegate: SearchViewController?
+    var selection: String = "Any"
     
     @IBAction func mileSlider(_ sender: UISlider) {
         mileSliderValue.text = String(Int(sender.value))
     }
-        
+    
     
     let pickerData = ["Any","Sports","Gaming","Hangout","Nightlife","Other"]
-
- 
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
-        }
-        
+        return 1
+    }
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return pickerData.count
-        }
+        return pickerData.count
+    }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)-> String?{
-            return pickerData[row]
-            
-        }
+        return pickerData[row]
+        
+    }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-          activityText.text = pickerData[row]
-        }
+        activityPickerTF.text = pickerData[row]
+    }
+
+
     
     
+    
+    @IBAction func beginSearchClicked(_ sender: Any) {
+//        delegate?.settingsChanged(activityText: activityText!.text!, mileSliderValue: mileSliderValue!.text!)
+//
+//        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    func createActivityPicker(){
+        activityPickerTF.inputView = activityPicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneActivityClicked))
+        toolbar.setItems([doneButton], animated: true)
+        activityPickerTF.inputAccessoryView = toolbar
+    }
+    @objc func doneActivityClicked(){
+        self.view.endEditing(true)
+    }
+    
+    @objc func hidePickers(){
+        self.view.endEditing(true)
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        activityPicker.delegate = self
+        createActivityPicker()
         
+    let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.self.hidePickers))
+        
+        self.view.addGestureRecognizer(detectTouch)
 
         // Do any additional setup after loading the view.
     }
